9777be4116eb6a1915125c910dee8beef8493d57
tjbdev <beornjade@gmail.com>
2022-10-30 19:57:40 +1100

    fix merge issues

--- sales/src/main/java/com/remotegroup/sales/domain/model/services/ISaleService.java
+++ sales/src/main/java/com/remotegroup/sales/domain/model/services/ISaleService.java
@@ -8,7 +8,6 @@ import com.remotegroup.sales.domain.model.aggregates.InStoreSale;
 import com.remotegroup.sales.domain.model.aggregates.OnlineSale;
 import com.remotegroup.sales.domain.model.aggregates.Sale;
 import com.remotegroup.sales.domain.model.aggregates.SaleId;
-<<<<<<< HEAD
 import com.remotegroup.sales.domain.model.aggregates.StoreId;
 import com.remotegroup.sales.domain.model.commands.CreateBackOrderSaleCommand;
 import com.remotegroup.sales.domain.model.commands.CreateInStoreSaleCommand;
@@ -19,14 +18,6 @@ import com.remotegroup.sales.domain.model.commands.UpdateOnlineSaleCommand;
 import com.remotegroup.sales.domain.model.commands.UpdateSaleCommand;
 import com.remotegroup.sales.exceptions.BackOrderSaleNotFoundException;
 import com.remotegroup.sales.shareddomain.Product;
-=======
-import com.remotegroup.sales.domain.model.commands.*;
-import com.remotegroup.sales.domain.model.valueobjects.ItemId;
-import com.remotegroup.sales.exceptions.*;
-import com.remotegroup.sales.shareddomain.*;
-
-import java.util.List;
->>>>>>> 7a650829a2a283a420d74b56f45cb54cbb24cb9a
 
 public interface ISaleService {
 	
